10e204980bc14a4ae5efe0d6c81f6371d3da5a0a
lightless <lightless@foxmail.com>
2016-07-27 00:30:53 +0800

    add coloredlogs

--- utils/data/LoggerHelp.py
+++ utils/data/LoggerHelp.py
@@ -3,21 +3,46 @@
 import logging
 import ConfigParser
 
+import coloredlogs
+import sys
+
 __author__ = "lightless"
 __email__ = "root@lightless.me"
 
+__all__ = ['logger']
 
-formatter = logging.Formatter("[%(levelname)s] [(%(threadName)s)] [%(asctime)s] [%(filename)s:%(lineno)d] %(message)s", "%H:%M:%S")
+FIELD_STYLES = dict(
+    asctime=dict(color='green'),
+    hostname=dict(color='magenta'),
+    levelname=dict(color='green', bold=coloredlogs.CAN_USE_BOLD_FONT),
+    filename=dict(color='magenta'),
+    name=dict(color='blue'),
+    threadName=dict(color='green')
+)
 
-console = logging.StreamHandler()
-console.setFormatter(formatter)
-
-logger = logging.getLogger("ProxySpiderLogger")
-logger.addHandler(console)
+LEVEL_STYLES = dict(
+    debug=dict(color='green'),
+    info=dict(color='cyan'),
+    verbose=dict(color='blue'),
+    warning=dict(color='yellow'),
+    error=dict(color='red'),
+    critical=dict(color='red', bold=coloredlogs.CAN_USE_BOLD_FONT)
+)
+logger = logging.getLogger("Pansidong")
 
 cf = ConfigParser.ConfigParser()
 cf.read("config.ini")
-if cf.has_option("ProxySpider", "debug") and cf.get("ProxySpider", "debug") == "On":
-    logger.setLevel(logging.DEBUG)
+if cf.has_option("Pansidong", "debug") and cf.get("Pansidong", "debug") == "On":
+    level = "DEBUG"
 else:
-    logger.setLevel(logging.INFO)
+    level = "INFO"
+
+coloredlogs.install(
+    level=level,
+    logger=logger,
+    stream=sys.stdout,
+    fmt="[%(levelname)s] [(%(threadName)s)] [%(asctime)s] [%(filename)s:%(lineno)d] %(message)s",
+    datefmt="%H:%M:%S",
+    level_styles=LEVEL_STYLES,
+    field_styles=FIELD_STYLES,
+)

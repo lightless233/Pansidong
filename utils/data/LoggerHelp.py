38f98ee52b5b05efbc2a56494bdca79947b1d53a
lightless <lightless@foxmail.com>
2016-07-27 22:22:40 +0800

    增加彩色log，fucking some log libraries.

--- utils/data/LoggerHelp.py
+++ utils/data/LoggerHelp.py
@@ -1,48 +1,30 @@
 #!/usr/bin/env python
 # coding: utf-8
-import logging
-import ConfigParser
 
-import coloredlogs
-import sys
+import colorlog
+import logging
 
 __author__ = "lightless"
 __email__ = "root@lightless.me"
 
 __all__ = ['logger']
 
-FIELD_STYLES = dict(
-    asctime=dict(color='green'),
-    hostname=dict(color='magenta'),
-    levelname=dict(color='green', bold=coloredlogs.CAN_USE_BOLD_FONT),
-    filename=dict(color='magenta'),
-    name=dict(color='blue'),
-    threadName=dict(color='green')
+
+handler = colorlog.StreamHandler()
+handler.setFormatter(
+    colorlog.ColoredFormatter(
+        fmt='%(log_color)s[%(levelname)s] [%(threadName)s] [%(asctime)s] [%(filename)s:%(lineno)d] %(message)s',
+        datefmt="%H:%M:%S",
+        log_colors={
+            'DEBUG': 'cyan',
+            'INFO': 'green',
+            'WARNING': 'yellow',
+            'ERROR': 'red',
+            'CRITICAL': 'red,bg_white',
+        },
+    )
 )
 
-LEVEL_STYLES = dict(
-    debug=dict(color='green'),
-    info=dict(color='cyan'),
-    verbose=dict(color='blue'),
-    warning=dict(color='yellow'),
-    error=dict(color='red'),
-    critical=dict(color='red', bold=coloredlogs.CAN_USE_BOLD_FONT)
-)
-logger = logging.getLogger("Pansidong")
-
-cf = ConfigParser.ConfigParser()
-cf.read("config.ini")
-if cf.has_option("Pansidong", "debug") and cf.get("Pansidong", "debug") == "On":
-    level = "DEBUG"
-else:
-    level = "INFO"
-
-coloredlogs.install(
-    level=level,
-    logger=logger,
-    stream=sys.stdout,
-    fmt="[%(levelname)s] [(%(threadName)s)] [%(asctime)s] [%(filename)s:%(lineno)d] %(message)s",
-    datefmt="%H:%M:%S",
-    level_styles=LEVEL_STYLES,
-    field_styles=FIELD_STYLES,
-)
+logger = logging.getLogger(__name__)
+logger.addHandler(handler)
+logger.setLevel("DEBUG")
